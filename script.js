2b4f0b32e249ad2ed4e405f6f89fbfb56f3df6e8
ventura <ventusDS@hotmail.com>
2023-04-17 03:39:32 -0600

    Agregando funcion para validar texto

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const copia = document.querySelector('.btn-copia');
 
 const resultado = document.querySelector('.respuesta');
 resultado.style.display = "none";
+
+function validarTexto() {
+    let textEscrito = document.getElementById('texto').value;
+    let validador = textoEscrito.match(/^[a-z]*$/);
+
+    if(!validador || validador === 0) {
+        alert("Solo son permitidas letras minúsculas y sin acentos");
+        location.reload();
+        return true;
+    }
+}
