4d6029b57591a9bc3bf2f618ebbd84ef618d1141
ventura <ventusDS@hotmail.com>
2023-04-17 03:31:19 -0600

    Creando variables con los selectores

---
+++ script.js
@@ -0,0 +1,6 @@
+const textUsuario = document.getElementById('texto');
+const textFinal = document.getElementById('textoFinal');
+const copia = document.querySelector('.btn-copia');
+
+const resultado = document.querySelector('.respuesta');
+resultado.style.display = "none";
